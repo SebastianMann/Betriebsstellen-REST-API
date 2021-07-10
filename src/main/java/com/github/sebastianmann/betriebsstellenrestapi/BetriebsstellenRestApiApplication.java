53879796e5705fe61c52a5dce6bc818c56720591
Sebastian Mann <mannsebi97@gmail.com>
2021-07-10 10:36:03 +0200

    initial commit, added initial spring setup

---
+++ src/main/java/com/github/sebastianmann/betriebsstellenrestapi/BetriebsstellenRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.sebastianmann.betriebsstellenrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetriebsstellenRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BetriebsstellenRestApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/sebastianmann/betriebsstellenrestapi/BetriebsstellenRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.sebastianmann.betriebsstellenrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BetriebsstellenRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
